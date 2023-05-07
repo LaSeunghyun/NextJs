679b7559fe636f209a2cf95d558c87aa92609794
LaSeunghyun <rmwl2356@gmail.com>
2023-05-07 21:11:23 +0900

    커밋테스트

--- app/not-found.js
+++ app/not-found.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
     return (
         <div>
-            <h2> 페이지 없어 이자식아 </h2>
+            <h2> 404다 페이지 없어 이자식아 </h2>
         </div>
     )
 }
