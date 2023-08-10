fc832c3c69cf63232d86ea0120cbdecd65567ab7
Raymond <akdfhr2@gmail.com>
2023-08-10 10:37:09 +0900

    HOTFIX: Board Api params 전달오류 수정

--- app/api/board/[id]/route.js
+++ app/api/board/[id]/route.js
@@ -1,29 +1,30 @@
 import { NextResponse } from "next/server";
 
-export function GET(req) {
+export function GET(_, { params }) {
+  const boardNum = params.id;
+  let now = new Date();
+  let year = now.getFullYear();
+  let month = now.getMonth() + 1;
+  let day = now.getDate();
+  const dateTime = year + " / " + month + " / " + day;
 
-    const url = new URL(req.url)
-    const boardNum = url.searchParams.get("id")
+  const text = [
+    {
+      idx: 0,
+      title: "제목",
+      content: "내용",
+      regdate: dateTime,
+      category: "잡담",
+    },
+    {
+      idx: 1,
+      title: "타이틀",
+      content: "와 됐어?",
+      regdate: dateTime,
+      category: "성공",
+    },
+  ];
 
-    let now = new Date();
-    let year = now.getFullYear();
-    let month = now.getMonth() + 1;
-    let day = now.getDate();
-    const dateTime = year + ' / ' + month + ' / ' + day;
-
-    const text = [{
-        "idx": 0,
-        "title": "제목",
-        "content": "내용",
-        "regdate": dateTime,
-        "category": "잡담"
-    },{
-        "idx": 1,
-        "title": "타이틀",
-        "content": "와 됐어?",
-        "regdate": dateTime,
-        "category": "성공"
-    }];
-    
-    return NextResponse.json(text[boardNum]);
+  const result = text[boardNum];
+  return NextResponse.json(result ?? { error: "Not Found." });
 }
